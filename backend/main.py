a05688e4a30db7ee4661035b247a0a519bf869e5
Pedro Lages <lula.noyb@gmail.com>
2026-06-17 14:34:36 +0100

    Allow Vercel origin in CORS

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,7 @@ app =  FastAPI()
 # CORS allow communication between frontend and backend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://final-project-pearl-six.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
